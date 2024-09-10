257ed1ae9737712efa0cb9ddedbf37140fb531c6
Seda07 <gevorgyanseda0@gmail.com>
2024-09-10 12:15:47 +0200

    DONE:modificed- evn, api, utl TODO: test connection

--- src/api.js
+++ src/api.js
@@ -1,22 +1,22 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
 
+// Crear una instancia de Axios
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+    baseURL: import.meta.env.VITE_API_URL, // URL base desde el archivo .env
+});
 
+// Interceptor de solicitud para agregar headers
 api.interceptors.request.use(
     (config) => {
-        config.headers["Content-Type"] = `application/json`
+        config.headers["Content-Type"] = "application/json";
         const token = localStorage.getItem(ACCESS_TOKEN);
         if (token) {
-            config.headers.Authorization = `Bearer ${token}`
+            config.headers.Authorization = `Bearer ${token}`;
         }
-        return config
+        return config;
     },
-    (error) => {
-        return Promise.reject(error)
-    }
-)
+    (error) => Promise.reject(error)
+);
 
-export default api
+export default api;
